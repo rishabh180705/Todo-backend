d592d051d57d77775f189caec22d135682e6b000
Rishabh Singh <coder.rishabh18@gmail.com>
2024-10-19 20:41:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,25 @@ import auth from './routes/auth.js';
 import list from './routes/list.js';
 import cors from 'cors'
 const app = express();
-app.use(cors({ origin:'https://rishabh180705.github.io/' }));
+
+const allowedOrigins = [
+    'https://rishabh180705.github.io/profile',
+    'https://rishabh180705.github.io/signin',
+    'https://rishabh180705.github.io/todo',
+    'https://rishabh180705.github.io/signup'
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type'
+}));
 
 app.use(express.json());
 //Routes
